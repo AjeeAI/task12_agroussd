ed1de3c2e3bd31042f31e6291d69087265fef170
Adesoji Ajijolaoluwa <ajeeaidev@gmail.com>
2025-09-05 21:07:00 +0100

    Another commit

--- main.py
+++ main.py
@@ -4,21 +4,32 @@ from agro_ussd.users.buyers import Buyer
 from managers.user_managers import register
 
 def main():
-    option1 = """
-Welcome to the Agro-Ussd system
-1. Login
-2. Register
-0. exit
-    """
-
     while True:
+        option1 = """
+    Welcome to the Agro-Ussd system
+    1. Login
+    2. Register
+    0. exit
+        """
+        print(option1)
         choice = int(input("Choose from the available options to continue: "))
         if choice == 0:
             break
         elif choice == 1:
             pass
         elif choice == 2:
+            option1 = """
+    Welcome to the Agro-Ussd system
+
+    1. Register as Buyer
+    2. Register as Farmer
+    0. Back
+    00. Exit
+        """
+            print(option1)
             register()
+        else:
+            print("Invalid option. Choose from the available ones!")
             
     
     
--- managers/user_managers.py
+++ managers/user_managers.py
@@ -8,14 +8,13 @@ Welcome to the Agro-Ussd system
 1. Register as Buyer
 2. Register as Farmer
 0. Back
-00. Exit
     """
     print(option1)
     while True:
         choice = int(input("Choose from the available options to continue: "))
-        if choice == 00:
-            print("Ussd is exiting!!!")
+        if choice == 0:
             break
+            
         elif choice == 1:
             print("-------------- Buyer Registration -------------")
             buyer = Buyer()
@@ -27,3 +26,9 @@ Welcome to the Agro-Ussd system
             farmer = Farmer()
             farmer.register()
             farmer.save_farmer()
+        else:
+            print("Invalid input. Please choose from the available options to continue!")
+            
+            
+# def login():
+    

3e70d5154227a9fce1b58736f51f606a3c7c16fc
bissakaAI <bissakabissakadop31.com>
2025-09-05 12:35:30 +0100

    splitted2

--- agro_ussd/users/farmers.py
+++ agro_ussd/users/farmers.py
@@ -23,8 +23,6 @@ from pathlib import Path
 import csv
 import random
 
-
-
 #i want to save userid and password in data base probably a csvfile
 workspace = Path("agro_ussd/data")
 workspace.mkdir(exist_ok=True)
@@ -87,26 +85,3 @@ def register_farm():
             
 
         print(user_details)
-#user registration
-#welcome message for the ussd service 
-print(("============="*6).center(100))
-print("WELCOME TO THE AGRO USSD SERVICE".center(100))
-print(("============="*6).center(100))
-
-#ask them who they are 
-enter_as = input("Press 1 if you are a farmer \nPress 2 if you are a user")
-if enter_as == "1":
-    acc_status = input("press 1 if you have an account\nPress 2 if you do not have an account")
-if acc_status == "1":
-    user_Id= input("Please input your Agronumber")
-    password_farmer= input("please input your password")
-
-randnum_used = [] # this need to be replaced with a file so that it doesnt get lost when program rerun 
-if acc_status == "2":
-    signup_status=sign_up()
-    if signup_status == "SUCCESS":
-        register_farm()
-#its either they signin or login
-#so i need to create a signin or login form for both user and farmer
-
-

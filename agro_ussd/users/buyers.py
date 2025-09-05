6e020dfcbd0f2b9edca2647f85de5157d587536a
Adesoji Ajijolaoluwa <ajeeaidev@gmail.com>
2025-09-05 18:59:59 +0100

    User class, buyer class and farmer class added and modified. User, buyer and farmers can now register on the platform

--- agro_ussd/users/buyers.py
+++ agro_ussd/users/buyers.py
@@ -1,6 +1,27 @@
-from users.user import User
+from user import User
+from pathlib import Path
+import csv
 
+folder = Path("agro_ussd/data/")
+path = folder / "buyers.csv"
 
 class Buyer(User):
-    def __init__(self, name, age, state, lga):
-        super().__init__(name, age, state, lga)
+    
+    def save_buyer(self):
+        file_exists = path.exists()
+        with open(path, "a", encoding="utf-8") as file:
+            fieldnames = ["name", "contact_info", "state", "lga"]
+            writer = csv.DictWriter(file, fieldnames = fieldnames)
+            if not file_exists:
+                writer.writeheader()
+                
+            writer.writerow({
+                "name": self.name,
+                    "contact_info": self.phone,
+                    "state": self.state,
+                    "lga": self.lga
+                    })
+buyer1 = Buyer()
+buyer1.register()
+buyer1.save_buyer()
+        
--- agro_ussd/users/user.py
+++ agro_ussd/users/user.py
@@ -1,9 +1,70 @@
+from pathlib import Path
+import csv
+
+folder = Path("agro_ussd/data/")
+path = folder / "users.csv"
+
+user_dict = {}
 class User:
-    def __init__(self, name, age, phone, state, lga):
+    def __init__(self, name= "", phone = "", state= "", lga = ""):
         self.name = name
-        self.age = age
         self.phone = phone
         self.state = state
         self.lga = lga
+        
+    def save_user(self):
+        file_exists = path.exists()
+        with open(path, "a", encoding="utf-8", newline = "") as file:
+            fieldnames = ["name", "contact_info", "state", "lga"]
+            writer = csv.DictWriter(file, fieldnames = fieldnames)
+            if not file_exists:
+                writer.writeheader()
+                
+            writer.writerow({
+                "name": self.name,
+                    "contact_info": self.phone,
+                    "state": self.state,
+                    "lga": self.lga
+                    })
+                
+            
+        
+    def register(self):
+        while True:
+            name = input("Enter your name: ").strip()
+            if name != "":
+                self.name = name
+            else:
+                print("This field cannot be left empty!!!")
+                continue
+                
+            phone = input("Enter your phone number: ").strip()
+            if phone != "":
+                self.phone = phone
+            else:
+                print("This field cannot be left empty!!!")
+                continue
+            state = input("Enter the state you reside in: ").strip()
+            if state != "":
+                self.state = state
+            else:
+                print("This field cannot be left empty!!!")
+                continue
+            
+            lga = input("Enter the lga of the area you reside: ").strip()
+            if lga != "":
+                self.lga = lga
+            else:
+                print("This field cannot be left empty!!!")
+                continue
+            print(user_dict)
+            break
+
+if __name__ == "__main__":
+    user1 = User()
+    user1.register()
+    user1.save_user()                
+            
+
         
     

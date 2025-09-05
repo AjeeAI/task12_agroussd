90e847d6d6b58265ce3850c95bdfedba8c8c7ebc
Adesoji Ajijolaoluwa <ajeeaidev@gmail.com>
2025-09-05 09:53:32 +0100

    Nothing really

--- task12_agroussd/agro_ussd/users/buyers.py
+++
@@ -1,6 +0,0 @@
-from users.user import User
-
-
-class Buyer(User):
-    def __init__(self, name, age, state, lga):
-        super().__init__(name, age, state, lga)
--- task12_agroussd/agro_ussd/users/user.py
+++
@@ -1,9 +0,0 @@
-class User:
-    def __init__(self, name, age, phone, state, lga):
-        self.name = name
-        self.age = age
-        self.phone = phone
-        self.state = state
-        self.lga = lga
-        
-    
